d6671ee1c5ad0e8246482a3b7da0177d0abe78ec
Eldar Guseynov <guseynoveldar3@gmail.com>
2024-02-27 19:07:56 +1000

    refactoring

--- src/page/MyDayPage/index.tsx
+++ src/page/MyDayPage/index.tsx
@@ -1,23 +1,11 @@
-import { Box, Heading, Icon, Stack, Text, useColorModeValue } from "@chakra-ui/react";
 import { FC, ReactElement } from "react";
 import { GoSun } from "react-icons/go";
-import { TodoTitle } from "../../compomemts/TodoTitle";
-import { CreateTask } from "../../compomemts/CreateTask";
 import { MY_DAY_LIST } from "../../constants/tasksListName";
-import { TaskList } from "../../compomemts/TaskList";
-import { TaskListMenu } from "../../compomemts/TaskListMenu";
+import { MainContent } from "../../compomemts/MainContent";
 
 
 export const MyDayPage: FC = (): ReactElement => {
-
-
    return (
-      <Box>
-         <TodoTitle title={'Мой день'} icon={GoSun} />
-         <CreateTask listName={MY_DAY_LIST} />
-         <TaskListMenu listName={MY_DAY_LIST} />
-         
-         
-      </Box>
+      <MainContent title={'Мой день'} icon={GoSun} listName={MY_DAY_LIST} />
    )
 }
