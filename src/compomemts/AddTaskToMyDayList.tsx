099e6879cad2faae0fbcf07fb661f5b99c94aa20
Eldar Guseynov <guseynoveldar3@gmail.com>
2024-03-07 11:46:14 +1000

    move task to my day list

--- src/compomemts/AddTaskToMyDayList.tsx
+++ src/compomemts/AddTaskToMyDayList.tsx
@@ -1,22 +1,56 @@
-import { Icon, Stack, Text } from "@chakra-ui/react";
-import { FC, ReactElement } from "react";
+import { Icon, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import { FC, ReactElement, useEffect, useState } from "react";
 import { IAddTaskToMyDayListProps } from "./types";
 import { GoSun } from "react-icons/go";
 import { RxCross1 } from "react-icons/rx";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../redux/store";
-import { addTaskInMyDayList, removeTaskInMyDayList } from "../redux/slices/tasksSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/store";
+import { addTaskInMyDayList, addTaskInTasksList, removeTaskInMyDayList } from "../redux/slices/tasksSlice";
+import { checkIsTaskInList } from "../utils/checkIsTaskInList";
+import { ADD_TASK_TO_MY_DAY_LIST, DELETE_TASK_TO_MY_DAY_LIST } from "../constants/addTaskToMyDayListTitle";
+import { ITask } from "../redux/types";
+import { getLocalDate } from "../utils/getLocalDate";
 
 
 export const AddTaskToMyDayList: FC<IAddTaskToMyDayListProps> = ({ task }): ReactElement => {
 
     const dispatch: AppDispatch = useDispatch();
 
-    function handlerForAddTaskToMyDayListBtn() {
-        if (task.list.isMyDayList) {
-            dispatch(removeTaskInMyDayList(task.id));
+    const taskLists = useSelector((state: RootState) => state.tasksSLice);
+
+    const titleInitValue: string = task.list.isMyDayList ? DELETE_TASK_TO_MY_DAY_LIST : ADD_TASK_TO_MY_DAY_LIST;
+    const [title, setTitle] = useState<string>(titleInitValue);
+
+    useEffect(() => {
+        if (checkIsTaskInList(taskLists.myDayList, task)) {
+            setTitle(DELETE_TASK_TO_MY_DAY_LIST);
         } else {
-            dispatch(addTaskInMyDayList(task));
+            setTitle(ADD_TASK_TO_MY_DAY_LIST);
+        }
+    }, [taskLists])
+
+    const textColor = useColorModeValue('gray.700', 'gray.300');
+    const hoverTextColor = useColorModeValue('black', 'white');
+
+    const updatedTask = (task: ITask): ITask => {
+        return {
+            ...task,
+            createdDate: {
+                year: getLocalDate().today.year,
+                month: getLocalDate().today.month,
+                day: getLocalDate().today.day,
+            },
+        }
+    }
+
+    function handlerForAddTaskToMyDayListBtn() {
+        if (checkIsTaskInList(taskLists.myDayList, task)) {
+            dispatch(removeTaskInMyDayList(task.id));
+            if (!checkIsTaskInList(taskLists.tasksList, task)) {
+                dispatch(addTaskInTasksList(task));
+            }
+        } else {
+            dispatch(addTaskInMyDayList(updatedTask(task)));
         }
     }
 
@@ -25,17 +59,16 @@ export const AddTaskToMyDayList: FC<IAddTaskToMyDayListProps> = ({ task }): Reac
             justify={'space-around'} 
             align={'center'} 
             cursor={'pointer'} 
+            color={task.list.isMyDayList ? 'blue.500' : textColor}
+            _hover={task.list.isMyDayList ? { color: 'blue.600' } : { color: hoverTextColor }}
             onClick={handlerForAddTaskToMyDayListBtn}
         >
             <Stack justify={'center'}>
                 <Icon as={GoSun} w={4} />
             </Stack>
             <Stack flex={'1 1 auto'} justify={'center'}>
-                <Text color={task.list.isMyDayList ? 'blue.500' : 'gray.400'}
-                    _hover={task.list.isMyDayList ? { color: 'blue.500' } : { color: 'black' }}>
-                    {
-                        task.list.isMyDayList ? 'Добавлено в список "Мой день"' : 'Добавить в список "Мой день"'
-                    }
+                <Text>
+                    {title}
                 </Text>
             </Stack>
             {
---
+++ src/constants/addTaskToMyDayListTitle.ts
@@ -0,0 +1,2 @@
+export const ADD_TASK_TO_MY_DAY_LIST = 'Добавить в список "Мой день"';
+export const DELETE_TASK_TO_MY_DAY_LIST = 'Добавлено в список "Мой день"';
