943a705a08f7def7db5c0b0fd0d346170a3841cd
Eldar Guseynov <guseynoveldar3@gmail.com>
2024-03-20 11:20:23 +1000

    refactoring menu icon

--- src/compomemts/TodoTitle.tsx
+++ src/compomemts/TodoTitle.tsx
@@ -1,13 +1,16 @@
-import { Stack, Icon, Heading, Text, useColorModeValue } from "@chakra-ui/react";
+import { Stack, Icon, Heading, Text, useColorModeValue, useMediaQuery } from "@chakra-ui/react";
 import { FC, ReactElement } from "react";
 import { ToggleMenuIcon } from "./ToggleMenuIcon";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { ITodoTitleProps } from "./types";
 import { getLocalDate } from "../utils/getLocalDate";
+import { WIDTH_1150_PX } from "../constants/windowWidth";
 
 
 export const TodoTitle: FC<ITodoTitleProps> = ({ icon, title }): ReactElement => {
+
+   const [isSmallerThan1150] = useMediaQuery(`(max-width: ${(WIDTH_1150_PX)})`);
 
    const dateTextColor = useColorModeValue('gray.600', 'gray.300');
 
@@ -21,9 +24,9 @@ export const TodoTitle: FC<ITodoTitleProps> = ({ icon, title }): ReactElement =>
       <Stack direction={'column'} p={4} gap={0} userSelect={'none'}>
          <Stack direction={'row'} align={'center'}>
             <ToggleMenuIcon isNavBar={false} />
-            {
-               isMenuOpen ? <Icon as={icon} boxSize={8} /> : <></>
-            }
+               {
+                  (isMenuOpen || isSmallerThan1150) ? <Icon as={icon} boxSize={8} /> : <></>
+               }
             <Heading as='h2' size={'md'}>{title}</Heading>
          </Stack>
          <Text color={dateTextColor} fontSize={'xs'}>
--- src/compomemts/ToggleMenuIcon.tsx
+++ src/compomemts/ToggleMenuIcon.tsx
@@ -1,10 +1,11 @@
-import { Box, Icon } from "@chakra-ui/react";
+import { Box, Icon, useMediaQuery } from "@chakra-ui/react";
 import { FC, ReactElement } from "react";
 import { FaHamburger } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { toggleMenu } from "../redux/slices/menuSlice";
 import { IToggleMenuIconProps } from "./types";
+import { WIDTH_1150_PX } from "../constants/windowWidth";
 
 
 export const ToggleMenuIcon: FC<IToggleMenuIconProps> = ({ isNavBar }): ReactElement => {
@@ -13,11 +14,14 @@ export const ToggleMenuIcon: FC<IToggleMenuIconProps> = ({ isNavBar }): ReactEle
 
    const isMenuOpen = useSelector((state: RootState) => state.menuSlice.isMenuOpen);
 
+   const [isSmallerThan1150] = useMediaQuery(`(max-width: ${(WIDTH_1150_PX)})`);
+
    function onToggleMenu() {
       dispatch(toggleMenu(!isMenuOpen));
    }
 
    if (isMenuOpen && !isNavBar) return <></>
+   if (isSmallerThan1150) return <></>
 
    return (
       <Box p={(!isMenuOpen && !isNavBar) ? 0 : 4}>
